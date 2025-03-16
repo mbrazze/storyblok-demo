4de0adc6d0595e3439aa7641bf4ac11403886d23
mbrazze <bryan.beale88@gmail.com>
2025-03-16 12:56:03 +0000

    image styling updated

--- us-cities-blog/components/City.js
+++ us-cities-blog/components/City.js
@@ -10,11 +10,17 @@ const City = ({ blok }) => {
       className="grid grid-cols-1 content-between gap-4 pb-20"
       {...storyblokEditable(blok)}
     >
-      <h1 className="text-center font-extrabold">{blok.Title}</h1>
-      <Link href={`/us-cities/${imageUrl}`}>
-        <img src={`${blok.image.filename}/m/736x414`} className="" />
-      </Link>
-      <p className="p-2">{cityContent}</p>
+      <h1 className="text-center font-extrabold pt-4">{blok.Title}</h1>
+      <div className="mx-auto max-w-4xl text-center ">
+        <Link href={`/us-cities/${imageUrl}`}>
+          <img
+            src={`${blok.image.filename}/m/736x414`}
+            alt={blok.image.alt || blok.Title}
+            className="block mx-auto w-full h-auto"
+          />
+        </Link>
+        <p className="p-2">{cityContent}</p>
+      </div>
     </div>
   );
 };
