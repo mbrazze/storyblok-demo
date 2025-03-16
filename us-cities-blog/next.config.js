cfec9263082c2853226980e3c4b7a79e1f3d111d
mbrazze <bryan.beale88@gmail.com>
2025-03-16 17:33:01 +0000

    locale updated

--- us-cities-blog/next.config.js
+++ us-cities-blog/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
-    // locales: ['en', 'es', 'de'], // or any array of language codes
+    locales: ['en'], // or any array of language codes
     defaultLocale: 'en',
   },
   // ...other config
