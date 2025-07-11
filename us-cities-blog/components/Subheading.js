e852ac524ab47456c6f7072ceecc5514ed67bd69
mbrazze <bryan.beale88@gmail.com>
2025-07-11 19:58:50 +0100

    subheading change

--- us-cities-blog/components/Subheading.js
+++ us-cities-blog/components/Subheading.js
@@ -6,7 +6,7 @@ const Subheading = ({ blok }) => {
       className="text-2xl mb-10 text-center font-bold pt-10"
       {...storyblokEditable(blok)}
     >
-      {blok.headline}
+      {blok.text}
     </h2>
   );
 };
